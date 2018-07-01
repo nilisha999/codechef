eaa6f81d17d36921ae94d563b92fdaea074569d4
nilisha999 <40057423+nilisha999@users.noreply.github.com>
2018-07-01 21:21:05 +0530

    Add files via upload

---
+++ Decine.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int t;
+		 if(a%4==0){
+		     int c=a+1;
+		     System.out.println(c);
+		 }
+		 else{
+		     int d=a-1;
+		     System.out.println(d);
+		 }
+	}
+}
