c9c63707cd8f9ea067a01725a1abe83b4c96021f
nilisha999 <40057423+nilisha999@users.noreply.github.com>
2018-12-23 20:29:14 +0530

    Add files via upload

---
+++ Ide (2).java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int N=sc.nextInt();
+		int r=sc.nextInt();
+		    for(int i=1;i<=N;i++){
+		            int R=sc.nextInt();
+		            if(R<r){
+		               System.out.println("Bad boi");
+		                   }
+		                    else{
+		                         System.out.println("Good boi");
+		                        }
+		    }
+	}
+}
