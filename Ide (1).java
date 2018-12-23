8d9cb8f327798d9e5c6275ef22d6f0e952ea0b8d
nilisha999 <40057423+nilisha999@users.noreply.github.com>
2018-12-23 19:13:22 +0530

    Add files via upload
    CHFIDEAL-the ideal problem of chef

---
+++ Ide (1).java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("2");
+	    System.out.flush();
+	    int Y=sc.nextInt();
+	    if(Y==3){
+	        System.out.println("1");
+	    }
+	    if(Y==1){
+	        System.out.println("3");
+	    }
+	}
+}
